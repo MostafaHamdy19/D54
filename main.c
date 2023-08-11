bbb0d0f8c660664ebdca0659641dc4dddcf6c189
MostafaHamdy19 <mostafa.hamdy11999@gmail.com>
2023-08-11 06:42:48 +0300

    Version 1

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x ,y;
+    printf("please enter your age: ");
+    scanf("%d",&x);
+    printf("the value is %d \n",x);
+
+    return 0;
+}
