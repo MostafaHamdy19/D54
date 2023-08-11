d2f535818a03fa2ceb4078dab3d489064c85c8ba
MostafaHamdy19 <mostafa.osha33@gmail.com>
2023-08-11 07:09:21 +0300

    Version 2

---
+++ file2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x ;
+    printf("please enter your age:10 ");
+    scanf("%d",&x);
+    printf("the value is %d \n",x);
+
+    return 0;
+}
